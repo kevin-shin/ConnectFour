28ee81d4123bc3dcc9bcd8e0b4f83ae24c82e52c
kevinshin <tshin@macalester.edu>
2019-02-24 20:27:48 -0600

    Complete with javadocs.

--- src/ConnectFour.java
+++ src/ConnectFour.java
@@ -3,15 +3,20 @@
  * Rent the Runway Take Home Code Challenge
  * Submitted 2/25
  *
- * ConnectFour.java is a program which executes a game of Connect4 between two players
- * through the console. Users are prompted to type in the column number to which they
- * would like to place their piece, and the turns alternate until
+ * ConnectFour.java is a program which executes a game of Connect4 between two players through the console.
+ * Users are prompted to type in the column number to which they would like to place their piece, and the
+ * turns alternate until a player wins, or a stalemate happens.
+ *
+ * The program is set up so that, by default, the program starts with player RED. Further, at the end of each game,
+ * the users are prompted to choose whether to start a new round.
  */
 
 import java.util.Scanner;
 
-
 public class ConnectFour {
+
+    /*********INNER CLASSES************/
+
     enum Color {
         RED, BLACK, EMPTY}
 
@@ -20,12 +25,10 @@ public class ConnectFour {
         public Player(Color color) {
             this.color = color;
         }
-
         public Color getColor() {
             return color;
         }
     }
-
 
     public ConnectFour(){};
 
@@ -33,8 +36,9 @@ public class ConnectFour {
     private int rowSize = 6;
     private Player player1;
     private Player player2;
-    private Player currentPlayer;
-    private boolean winner;
+    private Player currentPlayer; //Approach: keep track of which user has control over the board by toggling this
+                                  //instance variable.
+    private boolean winner; //true when a board presents a winning move, false otherwise.
     private Color[][] board = new Color[colSize][rowSize];
     private Scanner scanner = new Scanner(System.in);
 
@@ -44,7 +48,6 @@ public class ConnectFour {
             System.out.println("Invalid Board Params");
             return;
         }
-
         for (int i = 0; i < rowSize; i++) {
             Color[] col = new Color[colSize];
             for (int j = 0; j < colSize; j++) {
@@ -55,29 +58,17 @@ public class ConnectFour {
         player1 = new Player(Color.RED);
         player2 = new Player(Color.BLACK);
 
+        //set up variables as specified above
         currentPlayer = player1;
         winner = false;
     }
 
-    public void displayBoard() {
-        for (int i = 0; i < colSize; i++) {
-            System.out.print("  " + i + "   ");
-        }
-        System.out.println(" ");
-        for (int i = 0; i < rowSize; i++) {
-            for (int j = 0; j < colSize; j++) {
-                Color cell = board[i][j];
-                if (cell.equals(Color.RED)){
-                    System.out.print(" " + cell + "  ");
-                }
-                else {
-                    System.out.print(cell + " ");
-                }
-            }
-            System.out.println(" ");
-        }
-    }
 
+
+    /**
+     * Main wrapper function for getting the player's input, and checking whether the board is a winning move.
+     * The function itself is used mainly to print out prompts to guide the user.
+     */
     private void playerMove() {
         displayBoard();
         System.out.println(" ");
@@ -96,19 +87,9 @@ public class ConnectFour {
         }
     }
 
-    private boolean playable(){
-        for (int i = 0; i < rowSize; i++) {
-            for (int j = 0; j < colSize; j++) {
-                Color cell = board[i][j];
-                if (cell.equals(Color.EMPTY)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-
+    /**
+     * updateBoard() and legalWidth() take in the user input and places their piece in the proper place on the board.
+     */
     private void updateBoard() {
         while (true) {
             int col = legalWidth();
@@ -116,20 +97,10 @@ public class ConnectFour {
                 if (board[i][col].equals(Color.EMPTY)) {
                     board[i][col] = currentPlayer.getColor();
                     return;
-                    }
                 }
             }
         }
-
-    private void switchPlayer(){
-        if (currentPlayer.equals(player1)){
-            currentPlayer = player2;
-        }
-        else if (currentPlayer.equals(player2)){
-            currentPlayer = player1;
-        }
     }
-
     private int legalWidth() {
         while (true) {
             System.out.println(">>> ");
@@ -147,6 +118,7 @@ public class ConnectFour {
                 || checkDiagonal();
     }
 
+    /***************   CHECKERS FOR BOARD CONFIGURATION **************/
     private boolean checkHorizontal() {
         boolean win = false;
         Color[][] board = getBoard();
@@ -211,10 +183,6 @@ public class ConnectFour {
         }
     }
 
-    public Color[][] getBoard() {
-        return board;
-    }
-
     public static void main(String[] args) {
         ConnectFour cf = new ConnectFour();
         boolean playAgain = true;
@@ -223,12 +191,59 @@ public class ConnectFour {
             cf.initialize();
             cf.run();
             System.out.println("Play again? Yes/No");
-            String me = scanner.nextLine();
+            String input = scanner.nextLine();
 
-            if ((!me.equalsIgnoreCase("Yes"))){
+            if ((!input.equalsIgnoreCase("Yes"))){
                 playAgain = false;
             }
 
         }
     }
+
+    /**************    HELPER FUNCTIONS  **************/
+    public void displayBoard() {
+        for (int i = 0; i < colSize; i++) {
+            System.out.print("  " + i + "   ");
+        }
+        System.out.println(" ");
+        for (int i = 0; i < rowSize; i++) {
+            for (int j = 0; j < colSize; j++) {
+                Color cell = board[i][j];
+                if (cell.equals(Color.RED)){
+                    System.out.print(" " + cell + "  ");
+                }
+                else {
+                    System.out.print(cell + " ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+
+    private boolean playable(){
+        for (int i = 0; i < rowSize; i++) {
+            for (int j = 0; j < colSize; j++) {
+                Color cell = board[i][j];
+                if (cell.equals(Color.EMPTY)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void switchPlayer(){
+        if (currentPlayer.equals(player1)){
+            currentPlayer = player2;
+        }
+        else if (currentPlayer.equals(player2)){
+            currentPlayer = player1;
+        }
+    }
+
+    public Color[][] getBoard() {
+        return board;
+    }
+
+
 }
